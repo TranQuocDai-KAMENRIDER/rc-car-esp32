d57e3c1e130c1fd59e7b2d39d03120ce20f552e6
TranQuocDai-KAMENRIDER <tranquocdai06@gmail.com>
2026-01-20 10:08:21 +0700

    Delete src/steering_logic.h

--- src/steering_logic.h
+++
@@ -1,7 +0,0 @@
-#ifndef STEERING_LOGIC_H
-#define STEERING_LOGIC_H
-
-// Khai báo hàm tính toán tốc độ động cơ
-void calculateMotorSpeeds(int throttle, int steering, int &leftSpeed, int &rightSpeed);
-
-#endif
